25a947ee9863127385e739ee712df00e1ba8602c
VefBilge <vefugur@gmail.com>
2019-05-27 20:07:14 +0300

    Update SortbyOncelik.java

--- app/src/main/java/com/example/a15011082_dijitalnotdefteri/SortbyOncelik.java
+++ app/src/main/java/com/example/a15011082_dijitalnotdefteri/SortbyOncelik.java
@@ -5,6 +5,21 @@ import java.util.Comparator;
 public class SortbyOncelik implements Comparator<Note> {
     @Override
     public int compare(Note o1, Note o2) {
-        return o1.getOncelik().compareTo(o2.getOncelik());
+        String date1[],date2[];
+        date1 = o1.getTarih().split(" ");
+        date2 = o2.getTarih().split(" ");
+
+        int dateResultHour= date1[4].compareTo(date2[4]);
+        int dateResultDay =  date1[1].compareTo(date2[1]);
+
+        if (o1.getOncelik().compareTo(o2.getOncelik()) != 0)
+        {
+            return o1.getOncelik().compareTo(o2.getOncelik());
+        }else if(dateResultDay != 0 ){
+
+            return dateResultDay;
+        }
+        return dateResultHour;
     }
 }
+
